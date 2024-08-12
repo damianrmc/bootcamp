1012e5e0986102a1defaeae4a7f3eb300cfb83cf
damianrmc <damianrmc@gmail.com>
2024-08-12 09:05:48 -0400

    primera version

---
+++ CUAL ES MAYOR/assets/JS/javascript.js
@@ -0,0 +1,13 @@
+let numero1 = prompt("Introduzca un primer numero: ")
+
+let numero2 = prompt("Introduzca un segundo numero: ")
+
+if(numero1 > numero2){
+    alert("El primer numero es mayor que el segundo")
+}
+else if(numero1 < numero2){
+    alert("El segundo numero es mayor que el primero")
+}
+else{
+    alert("Los dos numeros son iguales")
+}
