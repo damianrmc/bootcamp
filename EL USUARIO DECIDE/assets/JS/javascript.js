48546f037ada16866649c0607461e3b05971bef8
damianrmc <damianrmc@gmail.com>
2024-08-13 03:15:36 -0400

    primera version

---
+++ EL USUARIO DECIDE/assets/JS/javascript.js
@@ -0,0 +1,10 @@
+function menu() {
+    let opcion = 0
+
+    while (opcion !== "5") {
+        opcion = prompt("Bienvenido, Seleccione una opción: \n 1.- Deposito \n 2.- Retiro \n 3.- Transferiencia \n 4.- Consulta de salto \n 5.- Salir")
+    }
+};
+
+
+menu();
